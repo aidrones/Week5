54460ba6a5cf039e5c3f063400b0a9f77cea059f
aidrones <35325464+aidrones@users.noreply.github.com>
2018-03-05 12:44:28 -0600

    Radio Buttons w/ Samsung Skins S4

--- app/src/main/java/com/radiobuttons/week5/MainActivity.kt
+++ app/src/main/java/com/radiobuttons/week5/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var txtName = findViewById<EditText>(R.id.txtName)
+        var txtName = findViewById<TextView>(R.id.txtName)
         var txtEmail = findViewById<EditText>(R.id.txtEmail)
         var txtShow = findViewById<TextView>(R.id.txtShow)
         var rbFemale = findViewById<RadioButton>(R.id.rbFemale)
